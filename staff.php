d29c3e4c47ee6f18f9aa8499678cee7dc56c9753
GEO-HP\ms <eep@geoffreyhk.com>
2018-11-04 12:05:29 +0800

    tset

--- staff.php
+++ staff.php
@@ -271,10 +271,11 @@ function ajaxAction(action) {
 				  {
 					$('#'+action+'_model').modal('hide');
 					$("#staff_grid").bootgrid('reload');
-
 					$('#frm_add').trigger("reset");
-
-				  }
+				},
+					fail:function() {
+	 					alert( response );
+ 					}
 				});
 			}
 
