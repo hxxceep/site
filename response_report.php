f494996717bc8096838c38194f66e800b5492de5
GEO-HP\ms <eep@geoffreyhk.com>
2018-12-09 15:44:54 +0800

    8

--- response_report.php
+++ response_report.php
@@ -129,7 +129,7 @@
 
 	while( $row2 = mysqli_fetch_row( $export2 ) )
 	{
-		$arry_paid[$row2[0]]= ",".$row2[1].",".$row2[2].",".$row2[3].",".$row2[4].",".$row2[5].",".$row2[6].",".$row2[7];
+		$arry_paid[$row2[0]]= ",".$row2[1].",".$row2[2].",".$row2[3].",".$row2[4].",".$row2[5].",".$row2[6].",".$row2[7].",".$row2[8];
 	}
 
 
@@ -152,7 +152,7 @@
 					if(isset($arry_paid[$sid])){
 					$data .= "\n".$name. $arry_paid[$sid];
 					}else{
-					$data .= "\n".$name .str_repeat(",",6);
+					$data .= "\n".$name .str_repeat(",",8 );
 					}
 					$i = 0;
 			   	foreach ($value as $key2 => $value2) {
